dd2f67bbb645d5dc9f47be1e491709426dc623a1
antknox <antknox@live.cn>
2019-07-07 17:44:10 +0800

    Add files via upload

---
+++ vxlast.bat
@@ -0,0 +1,20 @@
+@echo off 
+title 创建版本并发布到last
+
+:CJ
+echo 请输入版本号  (如:v1)
+set /p BB=
+if exist ..\%BB% echo 已有%BB%,请重新设置 & goto CJ
+
+:BB
+if not exist ..\%BB% md ..\%BB%
+copy /y .\*.* ..\%BB%
+
+exit
+
+:last
+if exist ..\last rd /s /q ..\last
+if not exist ..\last md ..\last
+if exist ..\last copy /y .\*.* ..\last
+
+start ..\last\ZM.bat
